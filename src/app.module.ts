ac1862adbbcd824f02b73eef93aaed89193062c3
Pedro-phd <pedro_dominici@outlook.com>
2024-07-15 23:54:58 -0300

    new schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { ConfigModule } from '@nestjs/config'
 import { MembersModule } from './domains/members/members.module'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaModule } from './prisma/prisma.module'
+import { DietModule } from './domains/diet/diet.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, MembersModule, PrismaModule],
+	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, MembersModule, PrismaModule, DietModule],
 	controllers: [AppController],
 	providers: [AppService, PrismaService],
 })
---
+++ src/domains/diet/diet.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common'
+import { CreateDietDto } from './dto/create-diet.dto'
+import { UpdateDietDto } from './dto/update-diet.dto'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Injectable()
+export class DietService {
+	constructor(private prisma: PrismaService) {}
+
+	create(createDietDto: CreateDietDto) {}
+
+	findAll() {
+		return 'This action returns all diet'
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} diet`
+	}
+
+	update(id: number, updateDietDto: UpdateDietDto) {
+		return `This action updates a #${id} diet`
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} diet`
+	}
+}
